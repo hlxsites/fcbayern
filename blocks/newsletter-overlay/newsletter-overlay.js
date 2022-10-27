367d8f4a8c047a04b69720ad4ae9fe8438758d84
Julien Ramboz <ramboz@adobe.com>
2022-10-27 15:49:50 +0200

    feat: add basic overlay logic

--- blocks/newsletter-overlay/newsletter-overlay.js
+++ blocks/newsletter-overlay/newsletter-overlay.js
@@ -1,4 +1,22 @@
 export default function decorate(block) {
-  // TODO: implement logic here
-  block.innerText += ' baz';
+
+  // The Close button
+  const closeButton = document.createElement('button');
+  closeButton.classList.add('newsletter-overlay-close');
+  closeButton.addEventListener('click', (ev) => {
+    block.parentElement.remove();
+  })
+  block.append(closeButton);
+
+  // Close when the overlay is clicked
+  block.parentElement.addEventListener('click', (ev) => {
+    if (ev.target.classList.contains('newsletter-overlay-wrapper')) {
+      block.parentElement.remove();
+    }
+  });
+
+  // Show the overlay after 2s
+  window.setTimeout(() => {
+    block.parentElement.classList.add('newsletter-overlay-wrapper--visible');
+  }, 2000);
 }
