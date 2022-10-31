a589654ea9be012de5e80acaeceeaa773821d50a
Mirko Sagolj <msagolj@adobe.com>
2022-10-31 19:30:17 +0100

    initial commit

--- scripts/liveticker.js
+++ scripts/liveticker.js
@@ -1,3 +1,5 @@
+import { getMetadata,createOptimizedPicture,decorateIcons } from "./scripts.js";
+
 const icons = {};
 
 const getIcon = async (name) => {
@@ -19,7 +21,7 @@ const getIcon = async (name) => {
   return icons[name];
 };
 
-const decorate = async () => {
+const decorateLiveTickerEntries = async () => {
   document.querySelectorAll('.liveticker main .section p, .liveticker main .section h2, .liveticker main .section h3').forEach(async (el) => {
     const container = document.createElement('div');
     const left = document.createElement('div');
@@ -56,4 +58,97 @@ const decorate = async () => {
   });
 };
 
-decorate();
+async function addLiveTickerHeader(main) {
+
+  // get metadata
+  const image = getMetadata("og:image");
+  const imageAlt = getMetadata("og:description");
+  const date = getMetadata("datum");
+  const time = getMetadata("zeit");
+  const matchday = getMetadata("matchtag");
+  const location = getMetadata("stadion");
+  const visitors = getMetadata("zuschauer");
+  const home = getMetadata("heimmannschaft");
+  const home_logo = getMetadata("heimmannschaft-logo");
+  const guest = getMetadata("gast");
+  const guest_logo = getMetadata("gast-logo");
+
+  // create DOM skeleton
+  const dom = document.createRange().createContextualFragment(`
+    <div class='liveticker-header'>
+      
+      <div class='image'>
+      </div>
+
+      <div class='matchinfo'>
+      
+        <div class='date-and-matchday'>
+          <div class='left'>${date}, ${time}</div>
+          <div>·</div>
+          <div class='right'>${matchday}</div>
+        </div>
+        
+        <div class='details'>
+          <div class='goals home'>
+          </div>
+          <div class='cards home'>
+          </div>
+          <div class='changes home'>
+          </div>
+          <div class='score-info'>
+            <div class='home-name'>${home}</div>
+            <div class='score'>
+              <div class='home-logo'></div>
+              <div class='goals'>2:0</div>
+              <div class='guest-logo'></div>
+            </div>
+            <div class='guest-name'>${guest}</div>
+          </div>
+          <div class='goals guest'>
+          </div>
+          <div class='cards guest'>
+          </div>
+          <div class='changes guest'>
+          </div>
+        </div>
+
+      </div>
+
+      <div class='location_visitiors'>
+        <div>Stadion/Zuschauerzahl</div>
+        <div>${location}</div>
+        <div>${visitors}</div>
+      </div>
+
+      <nav role='tablist'>
+        <div role='tab'>
+          Tore
+        </div>
+        <div role='tab'>
+          Karten
+        </div>
+        <div role='tab'>
+          Auswechslungen
+        </div>
+        <div role='tab'>
+          Spieleinfos
+        </div>
+      </nav>
+    </div>
+  `)
+
+  const picture = createOptimizedPicture(image, imageAlt, false, [
+    { media: '(max-width: 763px)', width: '320' },
+    { media: '(min-width: 764px) and (max-width: 1015px)', width: '640' },
+    { media: '(min-width: 1016px)', width: '1600' },
+  ]);
+  // add the image to the liveticker header from metadata
+  dom.querySelector('.image').append(picture);
+  main.prepend(dom);
+}
+
+export default function decorate(main) {
+  addLiveTickerHeader(main);
+  decorateLiveTickerEntries();
+}
+
