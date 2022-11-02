658aeda63e2bcfaeed6426a240638e91f4040d14
Mirko Sagolj <msagolj@adobe.com>
2022-11-02 12:19:03 +0100

    styling complete

--- scripts/liveticker.js
+++ scripts/liveticker.js
@@ -1,4 +1,4 @@
-import { getMetadata,createOptimizedPicture,decorateIcons } from "./scripts.js";
+import { getMetadata, createOptimizedPicture, decorateIcons } from './scripts.js';
 
 const icons = {};
 
@@ -59,19 +59,18 @@ const decorateLiveTickerEntries = async () => {
 };
 
 async function addLiveTickerHeader(main) {
-
   // get metadata
-  const image = getMetadata("og:image");
-  const imageAlt = getMetadata("og:description");
-  const date = getMetadata("datum");
-  const time = getMetadata("zeit");
-  const matchday = getMetadata("matchtag");
-  const location = getMetadata("stadion");
-  const visitors = getMetadata("zuschauer");
-  const home = getMetadata("heimmannschaft");
-  const home_logo = getMetadata("heimmannschaft-logo");
-  const guest = getMetadata("gast");
-  const guest_logo = getMetadata("gast-logo");
+  const image = getMetadata('og:image');
+  const desc = getMetadata('og:description');
+  const date = getMetadata('datum');
+  const time = getMetadata('zeit');
+  const matchday = getMetadata('matchtag');
+  const location = getMetadata('stadion');
+  const visitors = getMetadata('zuschauer');
+  const home = getMetadata('heimmannschaft');
+  const homeLogo = getMetadata('heimmannschaft-logo');
+  const guest = getMetadata('gast');
+  const guestLogo = getMetadata('gast-logo');
 
   // create DOM skeleton
   const dom = document.createRange().createContextualFragment(`
@@ -79,6 +78,11 @@ async function addLiveTickerHeader(main) {
       
       <div class='image'>
       </div>
+
+      <input name='tabs' type='radio' id='goals' checked='checked'>
+      <input name='tabs' type='radio' id='cards' >
+      <input name='tabs' type='radio' id='changes' >
+      <input name='tabs' type='radio' id='gameinfo' >
 
       <div class='matchinfo'>
         <div class='date-and-matchday'>
@@ -88,61 +92,94 @@ async function addLiveTickerHeader(main) {
         </div>
         
         <div class='details'>
-          <div class='goals home'>
-          <span class='icon icon-ball'></span>Hernández
+          <div class='goals-info home'>
+            <ol>
+              <li><span class='icon icon-ball'></span><b>51'</b> Hernández</li>
+              <li><span class='icon icon-ball'></span><b>54'</b> Sané</li>
+            </ol>
           </div>
-          <div class='cards home'>
-          cards home
+          <div class='cards-info home'>
+            <ol>
+              <li><b>19'</b> Marcel Sabitzer</li>
+              <li><b>74'</b> Joshua Kimmich</li>
+            </ol>
           </div>
-          <div class='changes home'>
-          <span class='icon icon-changes'></span>Noussair Mazraoui für Benjamin Pava
+          <div class='changes-info home'>
+            <ol>
+              <li><span class='icon icon-changes'></span><b>21'</b><span class='desc'>Noussair Mazraoui für Benjamin Pavard</span></li>
+              <li><span class='icon icon-changes'></span><b>46'</b><span class='desc'>Leon Goretzka für Marcel Sabitzer</span></li>
+              <li><span class='icon icon-changes'></span><b>69'</b><span class='desc'>Serge Gnabry für Sadio Mané</span></li>
+              <li><span class='icon icon-changes'></span><b>80'</b><span class='desc'>Ryan Gravenberch für Sané</span></li>
+              <li><span class='icon icon-changes'></span><b>80'</b><span class='desc'>Mathys Tel für Jamal Musiala</span></li>
+            </ol>         
           </div>
+
           <div class='score-info'>
             <div class='home-name'>${home}</div>
             <div class='score'>
               <div class='home-logo'></div>
-              <div class='goals'>2:0</div>
+              <div class='goals'>2 : 0</div>
               <div class='guest-logo'></div>
             </div>
             <div class='guest-name'>${guest}</div>
+            <div class='halftime'>(0:0)</div>
           </div>
-          <div class='goals guest'>
-          keine goals
+
+          <div class='goals-info guest'></div>
+          <div class='cards-info guest'>
+            <ol>
+              <li>Sergio Busquets <b>48'</b></li>
+            </ol>
           </div>
-          <div class='cards guest'>
-          keine cards
-          </div>
-          <div class='changes guest'>
-          keine changes
+          <div class='changes-info guest'>
+            <ol>
+              <li><span class='desc'>Ferran Torres für Raphinha</span><b>61'</b><span class='icon icon-changes'></span></li>
+              <li><span class='desc'>Frenkie de Jong für Gavi</span><b>70'</b><span class='icon icon-changes'></span></li>
+              <li><span class='desc'>Eric García für Andreas Christensen</span><b>80'</b><span class='icon icon-changes'></span></li>
+              <li><span class='desc'>Ansu Fati für Ousmane Dembélé</span><b>80'</b><span class='icon icon-changes'></span></li>
+              <li><span class='desc'>Franck Kessie für Sergio Busquets</span><b>80'</b><span class='icon icon-changes'></span></li>
+            </ol>
           </div>
         </div>
-
       </div>
 
       <div class='location-visitors'>
-        <div>Stadion/Zuschauerzahl</div>
+        <div class='title'>Stadion/Zuschauerzahl</div>
         <div>${location}</div>
-        <div>${visitors}</div>
+        <div>${visitors} Zuschauer</div>
       </div>
 
       <div class='tabs'>
         <nav role='tablist'>
-          <div role='tab'>Tore</div>
-          <div role='tab'>Karten</div>
-          <div role='tab'>Auswechslungen</div>
-          <div role='tab'>Spieleinfos</div>
+          <label class='goals' for='goals'>2 Tore</label>
+          <label class='cards' for='cards'>3 Karten</label>
+          <label class='changes' for='changes'>10 Auswechslungen</label>
+          <label class='gameinfo' for='gameinfo'>Spieleinfos</label>
         </nav>
       </div>
     </div>
-  `)
 
-  const headerimage = createOptimizedPicture(image, imageAlt, false, [
+    <div class='nav-bar'>
+      <button><span>Spielbericht</span></button>
+      <button><span>Galerie</span></button>
+      <button><span>Tabelle</span></button>
+      <button><span>FC Bayern.TV</span></button>
+      <button><span>Spieltag</span></button>
+      <button><span>Aufstellung</span></button>
+      <button class='active'><span>Liveticker</span></button>
+      <button><span>Vorbericht</span></button>
+      <button><span>Statistiken</span></button>
+      <button><span>News</span></button>
+    </div>
+  `);
+
+  const headerimage = createOptimizedPicture(image, desc, false, [
     { media: '(max-width: 763px)', width: '320' },
     { media: '(min-width: 764px) and (max-width: 1015px)', width: '640' },
     { media: '(min-width: 1016px)', width: '1600' },
   ]);
-  const homelogo = createOptimizedPicture(home_logo, home, false);
-  const guestlogo = createOptimizedPicture(guest_logo, guest, false);
+  const homelogo = createOptimizedPicture(homeLogo, home, false);
+  const guestlogo = createOptimizedPicture(guestLogo, guest, false);
 
   // add the image to the liveticker header from metadata
   dom.querySelector('.image').append(headerimage);
@@ -154,8 +191,27 @@ async function addLiveTickerHeader(main) {
   main.prepend(dom);
 }
 
+async function addTitleAndFilter(main) {
+  const desc = getMetadata('og:description');
+
+  // get the first section
+  const section = main.children[0];
+  const header = document.createRange().createContextualFragment(`
+    <div class='liveticker-filter' >
+      <h2>${desc}</h2>
+      <div>
+        <button class='active'><span>Alle Ergebnisse</span></button>
+        <button><span>2 Tore</span></button>
+        <button><span>3 Karten</span></button>
+        <button><span>10 Wechsel</span></button>
+      </div>
+    </div>
+  `);
+  section.prepend(header);
+}
+
 export default function decorate(main) {
   addLiveTickerHeader(main);
   decorateLiveTickerEntries();
+  addTitleAndFilter(main);
 }
-
